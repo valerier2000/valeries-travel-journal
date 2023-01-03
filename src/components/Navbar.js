faba452f299e6e4e8aa52c1b04b77e00ddbf1906
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2023-01-03 20:15:30 +0100

    Create Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <p>
+        <span class="fas fa-globe-americas"></span>
+        Valerie's travel journal.
+      </p>
+    </nav>
+  );
+}
