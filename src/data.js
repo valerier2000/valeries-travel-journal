4c86cba1085ccbf199e29e30d9be22d71c718bbd
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2023-01-03 20:10:58 +0100

    Set up project

---
+++ src/data.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    title: 'Desert "Wydma Łącka"',
+    location: "Poland, Łeba",
+    googleMapsUrl: "https://goo.gl/maps/QAmVbfABa2X9sTYn8",
+    startDate: "6 Aug, 2022",
+    description:
+      "Łączka (commonly known as Łącka Góra, also Łącka Wydma) - a dune hill with a variable height of about 30 m above sea level on the Łebska Spit, on the Słowiński Coast, located in the province of in the Pomeranian Voivodeship, in the Słupsk poviat, in the commune of Smołdzino. Łączka is located in the Słowiński National Park.",
+    img: "WydmaLacka.jpeg",
+    alt: "Desert",
+  },
+  {
+    title: "Gdańsk",
+    location: "Poland",
+    googleMapsUrl: "https://goo.gl/maps/2wifiKz93nuyKL7QA",
+    startDate: "4 Aug, 2022",
+    endDate: "5 Aug, 2022",
+    description:
+      "Gdańsk (Danzig in German) is a port city on the Baltic coast of Poland. At the center of its Main Town, reconstructed after WWII, are the colorful facades of Long Market, now home to shops and restaurants. Nearby is Neptune Fountain, a 17th-century symbol of the city topped by a bronze statue of the sea god. Gdańsk is also a center for the world’s amber trade; boutiques throughout the city sell the ossified resin.",
+    img: "Gdansk2.jpeg",
+    alt: "Gdansk",
+  },
+  {
+    title: "Malbork",
+    location: "Poland",
+    googleMapsUrl: "https://goo.gl/maps/pZiEsHLagZvigN3Y9",
+    startDate: "4 Aug, 2022",
+    description:
+      "Malbork is a town in the Pomeranian Voivodeship, Poland. It is the seat of Malbork County and has a population of 37,898 people as of 2021. The town is located on the Nogat river, in the historical region of Pomerelia.",
+    img: "Malbork.jpeg",
+    alt: "Malbork",
+  },
+  {
+    title: "Toruń",
+    location: "Poland",
+    googleMapsUrl: "https://goo.gl/maps/LApNAGCqgEQUmt3k8",
+    startDate: "3 Aug, 2022",
+    description:
+      "Toruń is a historical city on the Vistula River in north-central Poland and a UNESCO World Heritage Site. Its population was 196,935 as of December 2021. Previously, it was the capital of the Toruń Voivodeship and the Pomeranian Voivodeship.",
+    img: "Torun.jpeg",
+    alt: "Torun",
+  },
+  {
+    title: "Łódź",
+    location: "Poland",
+    googleMapsUrl: "https://goo.gl/maps/LGCGPWq7tX6ZDuZ46",
+    startDate: "3 Aug, 2022",
+    description:
+      "Łódź is a city in central Poland, known as a former textile-manufacturing hub. Its Central Museum of Textiles displays 19th-century machinery, fabrics and handicrafts linked to the trade. Once a factory, the restored Manufaktura complex is now a lively culture and arts center. Nearby is the grand Poznanski Palace, home to the City Museum, with artwork and objects depicting the history of Łódź.",
+    img: "Lodz.jpeg",
+    alt: "Lodz",
+  },
+];
